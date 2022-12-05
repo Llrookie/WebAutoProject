00da8ae1b328241b1432904e3c292620091a3479
Rookie <792932348@qq.com>
2022-12-05 23:14:35 +0800

    unittest.TestSuite()疑惑解答

--- UnitestStudy/study_unittest.py
+++ UnitestStudy/study_unittest.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 
@@ -13,9 +14,17 @@ class StudyUnittest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    suite = unittest.TestSuite()
-    suite.addTest(StudyUnittest("test_02_method"))
-    suite.addTest(StudyUnittest("test_01_method"))
-    unittest.TextTestRunner().run(suite)
     # 要想只执行添加到suite里的测试用例，需要注意右上角的执行环境，unittest***表示执行所有用例，需自己创建一个执行环境
     # https://zhuanlan.zhihu.com/p/97131025
+
+    suite = unittest.TestSuite()
+    # 一个一个测试用例添加
+    # suite.addTest(StudyUnittest("test_02_method"))
+    # suite.addTest(StudyUnittest("test_01_method"))
+    # unittest.TextTestRunner().run(suite)
+
+    # 批量添加用例，可以指定路径
+    testcases = unittest.TestLoader().discover(os.getcwd(),'study_unittest.py')
+    suite.addTests(testcases)
+    unittest.TextTestRunner().run(suite)
+
